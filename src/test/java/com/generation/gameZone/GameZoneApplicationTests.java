0b1c7eefd5b28eee785b524b1b893e506c61d167
Rodrigo-Santos-Dias <digoh.a4@gmail.com>
2024-07-10 10:56:27 -0300

    Commit de configuração do projeto

---
+++ src/test/java/com/generation/gameZone/GameZoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.gameZone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameZoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
