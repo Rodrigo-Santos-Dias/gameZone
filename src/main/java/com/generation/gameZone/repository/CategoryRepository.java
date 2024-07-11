dc8866e368c9041343dade6dc132739134e4cd7c
Rodrigo-Santos-Dias <digoh.a4@gmail.com>
2024-07-11 11:21:58 -0300

    finished

--- src/main/java/com/generation/gameZone/repository/CategoryRepository.java
+++ src/main/java/com/generation/gameZone/repository/CategoryRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface ProductRepository  extends JpaRepository <Products, Long> {
+public interface CategoryRepository extends JpaRepository <Products, Long> {
 
     public List<Products>findAllBynameContainsIgnoreCase(@Param("name")String name);
 }
